7a486ac7e804b35b19096ad68933430efeb048ba
vdanylo <danil.voronov@knu.ua>
2024-05-15 19:36:55 +0300

    commit

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,9 +5,9 @@ import { Footer } from "flowbite-react";
 export default function FooterWithLogo() {
   return (
     <Footer container>
-      <div className="w-full text-center">
-        <Footer.Copyright by="Omar Reda" year={2023} />
-      </div>
+      {/*<div className="w-full text-center">*/}
+      {/*  <Footer.Copyright by="Omar Reda" year={2024} />*/}
+      {/*</div>*/}
     </Footer>
   );
 }
